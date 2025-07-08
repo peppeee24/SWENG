c9fff4c126afd1f6798eb82c7b46b1b5ca211c71
Federico Sgambelluri <65596046+federicosgambelluri@users.noreply.github.com>
2025-07-08 16:12:52 +0200

    bug fix

--- backend/sweng/src/main/java/tech/ipim/sweng/SwengApplication.java
+++ backend/sweng/src/main/java/tech/ipim/sweng/SwengApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SwengApplication {
 
+	private SwengApplication() {
+		// Utility class
+	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(SwengApplication.class, args);
 		System.out.println("\n=== SWENG Backend Started ===");
