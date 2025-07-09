8a66b6e83aa1b8f88bfccf74cfc3914cd76ff08b
manuleo03 <manu.leooo03@gmail.com>
2025-07-09 18:23:38 +0200

    Aggiunta commenti frontend

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,10 +3,17 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 
+/**
+ * Configurazione globale dell'applicazione Angular
+ * 
+ * Qui vengono registrati i principali servizi (providers) e impostazioni che saranno
+ * disponibili a tutta l'app standalone.
+ */
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(withFetch())
+    provideZoneChangeDetection({ eventCoalescing: true }), // Configura il meccanismo di rilevamento dei cambiamenti di Angular
+    provideRouter(routes), // Registra il servizio di routing, con le route definite nel file app.routes.ts
+    provideHttpClient(withFetch()) // Registra il servizio HttpClient usando Fetch API al posto di XMLHttpRequest
   ]
 };
